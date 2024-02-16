e35e399fa39a25469fc10dfbecfa0242e0196fa4
luisAlva2004 <luisgera.alvarado@gmail.com>
2024-02-16 13:17:03 -0600

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var http = require("http");
 
-const datos = JSON.stringify({nombre:'Luis Gerardo Alvarado Moreno', grupo:'5A', matricula:'20963'})
-
 http.createServer(function (request, response){
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(datos);
+    response.end('Hola Mundo');
 }).listen(8080);
 console.log('Server running at http://127.0.0.1:8080/');
